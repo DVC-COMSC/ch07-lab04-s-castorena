25a73ea7bfcf9235bcb15699bb3a27e87d3e9880
sarah castorena <ceanz@yahoo.com>
2022-10-11 20:22:17 -0700

    new commit

--- main.py
+++ main.py
@@ -10,5 +10,10 @@ result = []
 for i in range(10):
 	numbers1.append(random.randint(0,100))
 	numbers2.append(random.randint(0,100))
+
+for i in range(len(numbers1)):
+		result.append(numbers1[i] + numbers2[i])
+
 print (numbers1)
 print (numbers2)
+print (result)
